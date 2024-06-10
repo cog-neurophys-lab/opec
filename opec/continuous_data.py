42869767ffdc1d0d5b598f9036060f3c69b1fbf1
Joscha Schmiedt <schmiedt@uni-bremen.de>
2024-06-10 15:30:48 +0200

    Deduce number of channels from data size and number of samples

--- opec/continuous_data.py
+++ opec/continuous_data.py
@@ -16,8 +16,6 @@ class OpenEphysContinuousData:
     def from_message(data: bytes, header: ContinuousDataHeaderMessage):
 
         n_arr = np.frombuffer(data, dtype=OpenEphysContinuousData.dtype)
-        n_arr = np.reshape(
-            n_arr, (header.content.channel_num, header.content.num_samples)
-        )
+        n_arr = np.reshape(n_arr, (-1, header.content.num_samples))
 
         return OpenEphysContinuousData(array=n_arr, header=header)
