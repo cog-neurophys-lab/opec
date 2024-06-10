33e70c3b741488cd8d751c1d82b4d42c1571f34c
Joscha Schmiedt <schmiedt@uni-bremen.de>
2024-06-10 12:36:57 +0200

    Move playground to opec/__init.py__ (make module executable)

--- opec/__init__.py
+++ opec/__init__.py
@@ -1,3 +1,23 @@
 from .connection import Connection
 from .messages import HeartBeatMessage
 from .client import Client
+from .circular_buffer import CircularBuffer
+
+
+__all__ = ["Connection", "HeartBeatMessage", "Client", "CircularBuffer"]
+
+ZMQ_PLUGIN_MIN_VERSION = ["0.3.0"]
+
+
+def main():
+    import logging
+
+    logger = logging.getLogger("logger")
+    logger.setLevel(logging.DEBUG)
+
+    c = Client(app_name="Python Test Client")
+    c.loop()
+
+
+if __name__ == "__main__":
+    main()
